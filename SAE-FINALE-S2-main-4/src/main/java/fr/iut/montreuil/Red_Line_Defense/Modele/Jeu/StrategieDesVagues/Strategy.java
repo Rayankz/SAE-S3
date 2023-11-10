17baab3f01d2279ec84fc630fcda40ab28eff837
aabdi <arsanbosss@gmail.com>
2023-11-10 15:29:04 +0100

    problemes sur la listObs des tours qui persistent

--- SAE-FINALE-S2-main-4/src/main/java/fr/iut/montreuil/Red_Line_Defense/Modele/Jeu/StrategieDesVagues/Strategy.java
+++ SAE-FINALE-S2-main-4/src/main/java/fr/iut/montreuil/Red_Line_Defense/Modele/Jeu/StrategieDesVagues/Strategy.java
@@ -1,6 +1,7 @@
 package fr.iut.montreuil.Red_Line_Defense.Modele.Jeu.StrategieDesVagues;
 
 public abstract class Strategy {
+
     /*private ForgeSoldats forgeSoldats;
 
     public Strategy(ForgeSoldats forgeSoldats) {
