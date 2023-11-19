e3afc47a8aa1fb938d3cd197ab1f04f8d317d913
Rayankz <kherouarayan@gmail.com>
2023-11-19 14:55:22 +0100

    bjbhjb test push

--- SAE-FINALE-S2-main-4/src/main/java/fr/iut/montreuil/Red_Line_Defense/Controleurs/ControleurDefaite.java
+++ SAE-FINALE-S2-main-4/src/main/java/fr/iut/montreuil/Red_Line_Defense/Controleurs/ControleurDefaite.java
@@ -5,13 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
 import javafx.scene.media.Media;
-
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -47,7 +44,6 @@ public class ControleurDefaite implements Initializable {
         System.out.println("Media arrêté");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/iut/montreuil/Red_Line_Defense/Vues/vueMenuPrincipal.fxml"));
         Parent roott = loader.load();
-        ControleurMP controleur = loader.getController(); // Retrieve the controller instance
         stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(roott, 940, 560);// Largeur 940px : 840px pour la carte, 100px pour le volet droit
         stage.setResizable(false);                     // Hauteur 560px : 480 pour la carte, 80px pour le volet bas
